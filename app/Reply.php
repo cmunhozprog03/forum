e071d457199335cd6289e1ee576f9bd8447b0093
cmunhozprog03h <cmunhozprog03@hotmail.com>
2021-02-03 16:41:54 -0300

    Migrate

--- app/Reply.php
+++ app/Reply.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Replay extends Model
+class Reply extends Model
 {
     //
 }
--- database/migrations/2021_02_03_193405_create_replies_table.php
+++ database/migrations/2021_02_03_193405_create_replies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateThreadsTable extends Migration
+class CreateRepliesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,18 +13,17 @@ class CreateThreadsTable extends Migration
      */
     public function up()
     {
-        Schema::create('threads', function (Blueprint $table) {
+        Schema::create('replies', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('thread_id');
             $table->unsignedBigInteger('user_id');
 
-            $table->string('title');
-            $table->text('body');
-            $table->string('slug');
-
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-
-
+            $table->text('replay');
+            
             $table->timestamps();
+
+            $table->foreign('thread_id')->references('id')->on('threads')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
@@ -35,6 +34,6 @@ class CreateThreadsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('threads');
+        Schema::dropIfExists('replies');
     }
 }
